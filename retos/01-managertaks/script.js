e713a4a89a04d394b408a28de2debaaa9daabcf4
cirorey20 <cirorey95@gmail.com>
2025-01-06 18:57:56 -0300

    add feature remove task

--- retos/01-managertaks/script.js
+++ retos/01-managertaks/script.js
@@ -1,15 +1,22 @@
-//capturar el imput de nueva tarea
+//capturar el input de nueva tarea
 let textTask = document.getElementById('inputTask');
-
 //capturar el contenedor de lista de tareas
 let newTask = document.getElementById('newTask');
+//capturar el contenedor del botón para crear nueva tarea
 let addNewStaks = document.getElementById('addbutton');
 
-//agregar tareas nuevas
+//agregar y eliminar tareas nuevas
 addNewStaks.addEventListener('click', () => {
+    // Crear la nueva tarea
     const nuevaTarea = document.createElement('li');
-    // console.log(!textTask.value == "" ? false: true);
-    nuevaTarea.textContent= textTask.value;
+    nuevaTarea.textContent = textTask.value; 
+    
+    // Crear botón de eliminar en la nueva tarea
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Eliminar';
+
+    // Agregar el botón a la tarea
+    nuevaTarea.appendChild(deleteButton); 
 
     if (!textTask.value == "") {
         console.log(true);
@@ -19,5 +26,15 @@ addNewStaks.addEventListener('click', () => {
         console.log(false);
     }
     
+    //Eliminar tarea
+    deleteButton.addEventListener('click', () => {
+        newTask.removeChild(nuevaTarea);
+    })
 });
 
+
+
+
+// for(taks of listTasks.children) {
+//     console.log(taks.innerText);
+// }
