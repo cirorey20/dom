0940a92a4d0d9f6d81f74cad18d1790eac082a19
cirorey20 <cirorey95@gmail.com>
2025-01-06 13:15:01 -0300

    firts commit

---
+++ retos/01-managertaks/script.js
@@ -0,0 +1,11 @@
+let newTask = document.getElementById('newTask');
+let add = document.getElementById('add');
+
+let addButton = addEventListener('click', () => {
+    const nuevaTarea = document.createElement('p');
+    console.log("newTask");
+    nuevaTarea.textContent= "Nueva Tarea Agregada";
+
+    newTask.appendChild(nuevaTarea);
+})
+
