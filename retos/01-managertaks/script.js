734e4956e62480326d5a1a4b72f9cd65edb1f58c
cirorey20 <cirorey95@gmail.com>
2025-01-07 14:02:40 -0300

    add new feature: checkButton

--- retos/01-managertaks/script.js
+++ retos/01-managertaks/script.js
@@ -9,26 +9,38 @@ let addNewStaks = document.getElementById('addbutton');
 addNewStaks.addEventListener('click', () => {
     // Crear la nueva tarea
     const nuevaTarea = document.createElement('li');
-    nuevaTarea.textContent = textTask.value; 
-    
+    nuevaTarea.textContent = textTask.value;
+
     // Crear botón de eliminar en la nueva tarea
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Eliminar';
 
-    // Agregar el botón a la tarea
-    nuevaTarea.appendChild(deleteButton); 
+    // Agregar el botón de eliminar a la tarea
+    nuevaTarea.appendChild(deleteButton);
 
-    if (!textTask.value == "") {
-        console.log(true);
+    // Crear botón de marcar tarea como completada
+    const checkButton = document.createElement('button');
+    checkButton.textContent = 'Completada';
+
+    // Agregar el botón de check a la tarea
+    nuevaTarea.appendChild(checkButton);
+
+    if (!textTask.value == "") { //valida si hay texto o no
         newTask.appendChild(nuevaTarea); //agregar tareas nuevas
         textTask.value = "";
     } else {
-        console.log(false);
+        console.log("El input está vacio");
     }
-    
+
     //Eliminar tarea
     deleteButton.addEventListener('click', () => {
         newTask.removeChild(nuevaTarea);
+    });
+
+    //Completar tarea
+    checkButton.addEventListener('click', () => {
+        nuevaTarea.style.textDecorationLine = 'line-through';  // Tachado
+        nuevaTarea.style.textDecorationStyle = 'solid';
     })
 });
 
